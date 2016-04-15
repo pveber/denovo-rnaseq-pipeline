879795fd58aba855cb922d68d173cb910fe1b610
pveber <philippe.veber@gmail.com>
2016-04-15 16:39:54 +0200

    fixed trinity call

--- src/trinity.ml
+++ src/trinity.ml
@@ -4,14 +4,16 @@ open Bistro_bioinfo.Std
 open Bistro.EDSL_sh
 
 let trinity fa1 fa2 =
+  let dest = dest // "trinity" in
   workflow ~descr:"trinity" ~np:10 ~mem:(140 * 1024) [
+    mkdir_p dest ;
     cmd "Trinity" [
       string "--verbose" ;
       string "--seqType fa" ;
       opt "--left" dep fa1 ;
       opt "--right" dep fa2 ;
       opt "--CPU" ident np ;
-      opt "--max_memory" ident (seq [ string "$((" ; mem ; string " / 1024))" ]) ;
+      opt "--max_memory" ident (seq [ string "$((" ; mem ; string " / 1024))G" ]) ;
       opt "--output" ident dest ;
     ]
   ]
