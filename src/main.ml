3b569d912e8d0055c9fabc5535871376b640422d
pveber <philippe.veber@gmail.com>
2016-04-15 17:17:45 +0200

    only dump workflow instead of executing it

--- src/main.ml
+++ src/main.ml
@@ -44,19 +44,14 @@ let pipeline preview_mode fq1_path fq2_path =
     [ "Trinity" ] %> trinity_assembly ;
   ]
 
-let main preview_mode outdir tmpdir np mem fq1_path fq2_path () =
-  let backend = Bistro_engine.Scheduler.local_backend ?tmpdir ~np ~mem:(mem * 1024) () in
+let main preview_mode fq1_path fq2_path () =
   let targets = pipeline preview_mode fq1_path fq2_path in
-  Bistro_app.with_backend ~outdir backend targets
+  Bistro_app.plan_to_channel targets stdout
 
 let spec =
   let open Command.Spec in
   empty
   +> flag "--preview-mode" no_arg ~doc:" Run on a small subset of the data"
-  +> flag "--outdir"  (required string) ~doc:"DIR Directory where to link exported targets"
-  +> flag "--tmpdir"  (optional string) ~doc:"DIR (Preferably local) directory for temporary files"
-  +> flag "--np"      (optional_with_default 4 int) ~doc:"INT Number of processors"
-  +> flag "--mem"     (optional_with_default 4 int) ~doc:"INT Available memory (in GB)"
   +> anon ("FQ1" %: file)
   +> anon ("FQ2" %: file)
 
