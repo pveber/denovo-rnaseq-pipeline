46d082353ace7d998bddad40b845843b1ec2657b
pveber <philippe.veber@gmail.com>
2016-05-14 15:48:38 +0200

    trinity: arg to control memory consumption

--- src/trinity.ml
+++ src/trinity.ml
@@ -3,9 +3,9 @@ open Bistro.Std
 open Bistro_bioinfo.Std
 open Bistro.EDSL_sh
 
-let trinity fa1 fa2 =
+let trinity ?(mem = 128) fa1 fa2 =
   let tmp_dest = tmp // "trinity" in
-  workflow ~descr:"trinity" ~np:10 ~mem:(140 * 1024) [
+  workflow ~descr:"trinity" ~np:10 ~mem:(mem * 1024) [
     mkdir_p tmp ;
     cmd "Trinity" [
       string "--verbose" ;
@@ -13,7 +13,7 @@ let trinity fa1 fa2 =
       opt "--left" dep fa1 ;
       opt "--right" dep fa2 ;
       opt "--CPU" ident np ;
-      opt "--max_memory" ident (seq [ string "$((" ; mem ; string " / 1024))G" ]) ;
+      opt "--max_memory" ident (seq [ string "$((" ; Bistro.EDSL_sh.mem ; string " / 1024))G" ]) ;
       opt "--output" ident tmp_dest ;
     ] ;
     cmd "mv" [
